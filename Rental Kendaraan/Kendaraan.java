c87da11f7cb4b335611163fe3d502bf2e07a0d81
awokowkw <new.monster.baby@gmail.com>
2025-11-04 12:00:59 +0700

    Create Kendaraan.java

---
+++ Rental Kendaraan/Kendaraan.java
@@ -0,0 +1,30 @@
+public class Kendaraan
+{
+    private String merk;
+    private String model;
+    private int tahunProduksi;
+    
+    public Kendaraan(String merk, String model, int tahunProduksi)
+    {
+        this.merk = merk;
+        this.model = model;
+        this.tahunProduksi = tahunProduksi;
+    }
+    
+    public String getMerk(){
+        return merk;
+    }
+
+    public String getModel(){
+        return model;
+    }
+    
+    public int getTahunProduksi(){
+        return tahunProduksi;
+    }
+    
+    @Override
+    public String toString(){
+        return merk + " " + model + " (" + tahunProduksi + ")";
+    }
+}
