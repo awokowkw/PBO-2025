5a2e2eba621ceae9fa9e40d4d682be3a7c4155a7
awokowkw <new.monster.baby@gmail.com>
2025-12-08 10:09:11 +0700

    Create makhlukHidup.java

---
+++ Abstraksi Makhluk Hidup/makhlukHidup.java
@@ -0,0 +1,10 @@
+public abstract class MakhlukHidup {
+   
+   public abstract void berkembangBiak ();
+   
+   public void bertahanHidup(){
+       System.out.println("- membutuhkan makanan");
+       System.out.println("- membutuhkan oksigen");
+       System.out.println("- membutuhkan air");
+   }     
+}
