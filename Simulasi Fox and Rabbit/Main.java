ff122b95582e8317d49342a43278dcd0680a2f21
awokowkw <new.monster.baby@gmail.com>
2025-12-08 09:43:23 +0700

    Create Main.java

---
+++ Simulasi Fox and Rabbit/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== FOX & RABBIT SIMULATION ===");
+        
+        Simulator simulator = new Simulator(5, 5); 
+        simulator.simulate(5);                      
+
+    }
+}
