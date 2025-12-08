38a9810d49649817a4dc8af9c96ecbcc7213527f
awokowkw <new.monster.baby@gmail.com>
2025-12-08 10:09:42 +0700

    Create Tumbuhan.java

---
+++ Abstraksi Makhluk Hidup/Tumbuhan.java
@@ -0,0 +1,11 @@
+public class Tumbuhan extends MakhlukHidup {
+    private String Rizoma;
+    
+    public Tumbuhan (String Rizoma){
+        this.Rizoma = Rizoma;
+    }
+    
+    public void berkembangBiak (){
+        System.out.println("Jahe berkembang biak dengan     :  "+Rizoma);
+    }
+}
